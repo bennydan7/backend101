18fca568d17b9f0c4fd8570b108a3079a249ac0b
bennydan7 <owusub243@gmail.com>
2024-11-24 10:04:11 +0000

    update

--- express-project/server.js
+++ express-project/server.js
@@ -3,11 +3,26 @@ import express from 'express';
 const app = express();
 const PORT = 3000
 
-app.get('/',(req,res)=>{
-    res.send({
-        id: 1,
-        name : 'John Doe'
-    })
+const friends = [
+    {
+    id : 0,
+    name: 'John Nolan',
+},
+{
+    id : 1,
+    name: 'Lucy Chen'
+}]
+
+app.get('/friends', (req,res)=>{res.json(friends)})
+
+app.get('/friends/:friendId',(req,res)=>{
+    const friendId = Number(req.params.friendId)
+    const friend = friends[friendId]
+    if(friend){
+        res.json(friend)
+    }else{
+        res.status(404).json({error: 'Friend not found'})
+    }
 })
 
 app.get('/messages',(req,res)=>{
