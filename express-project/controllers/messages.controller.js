094a710b807a1c030c4e6ba7fca7f699330c8412
bennydan7 <owusub243@gmail.com>
2024-11-25 11:27:47 +0000

    update

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,12 +1,20 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Simulate __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const getMessages = (req, res) => {
-    res.send('<ul><li>The Rookie</li><li>10/10</li></ul>');
-}
+    res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpg'));
+    // res.send('<ul><li>The Rookie</li><li>10/10</li></ul>');
+};
 
 function postMessages(req, res) {
-    console.log("Updating messages....")
+    console.log("Updating messages...");
 }
 
 export default {
     getMessages,
-    postMessages
+    postMessages,
 };
