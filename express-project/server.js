6df66a57f167935ef46ee6a5539d487d129d8dba
bennydan7 <owusub243@gmail.com>
2024-11-25 11:09:02 +0000

    update

--- express-project/server.js
+++ express-project/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import messagesController from './controllers/messages.controller.js';  // Importing the default export
 import friendsController from './controllers/friends.controller.js';
+import friendsRouter from './routes/friends.router.js';
+import messagesRouter from './routes/messages.router.js';
 
 const { getMessages, postMessages } = messagesController;
 const { postFriends, getFriends, getFriend } = friendsController;
@@ -14,18 +16,15 @@ app.use((req, res, next) => {
     const start = Date.now();
     next();
     const delta = Date.now() - start;
-    console.log(`${req.method} ${req.url} ${delta}ms`);
+    console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
 app.use(express.json());
 
-app.post('/friends',friendsController.postFriends)
-app.get('/friends',friendsController.getFriends);
-app.get('/friends/:friendId',friendsController.getFriend
-);
+app.use('/friends',friendsRouter)
+app.use('/messages',messagesRouter)
 
-app.get('/messages', getMessages); 
-app.post('/messages', postMessages);  
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
