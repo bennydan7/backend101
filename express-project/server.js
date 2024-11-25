cf702c07a93dc9ee374e22948d93ee7b289c66ee
bennydan7 <owusub243@gmail.com>
2024-11-25 11:45:11 +0000

    update

--- express-project/server.js
+++ express-project/server.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import path from 'path';
 import messagesController from './controllers/messages.controller.js';  // Importing the default export
 import friendsController from './controllers/friends.controller.js';
 import friendsRouter from './routes/friends.router.js';
 import messagesRouter from './routes/messages.router.js';
+
+
+import { fileURLToPath } from 'url';
+
+// Simulate __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const { getMessages, postMessages } = messagesController;
 const { postFriends, getFriends, getFriend } = friendsController;
@@ -19,6 +27,7 @@ app.use((req, res, next) => {
     console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
+app.use('/site',express.static(path.join(__dirname,'public')));
 app.use(express.json());
 
 app.use('/friends',friendsRouter)
