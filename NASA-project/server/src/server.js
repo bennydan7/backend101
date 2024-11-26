669353612c40db9739257479312748b95db5fac5
bennydan7 <owusub243@gmail.com>
2024-11-26 23:04:24 +0000

    modified server

--- NASA-project/server/src/server.js
+++ NASA-project/server/src/server.js
@@ -1,13 +1,10 @@
 import http from 'http';
-
-import app from './app';
+import app from './app.js';
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
-
-
+});
