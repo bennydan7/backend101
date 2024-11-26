0f7793b8c1c8e790e38c6dc62c7b9ed7c1671dbc
bennydan7 <owusub243@gmail.com>
2024-11-26 23:03:55 +0000

    modified express server

--- NASA-project/server/src/app.js
+++ NASA-project/server/src/app.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import planetsRouter from './routes/planets/planets.router.js';
 
 const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Use planets router
+app.use('/planets', planetsRouter);
 
 export default app;
