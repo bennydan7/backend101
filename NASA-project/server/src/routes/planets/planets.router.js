f1a74f3badad628d963d0bc27d7db4b0808b288a
bennydan7 <owusub243@gmail.com>
2024-11-26 23:03:07 +0000

    :

---
+++ NASA-project/server/src/routes/planets/planets.router.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { getAllPlanets } from './planets.controller.js'; // Correct import
+
+const planetsRouter = express.Router();
+
+// Define the route
+planetsRouter.get('/', getAllPlanets);
+
+export default planetsRouter;
