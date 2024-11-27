9e1f55043e10809c79a73c6e51e3a98670976383
bennydan7 <owusub243@gmail.com>
2024-11-27 16:19:10 +0000

    added cors middleware

--- NASA-project/server/src/app.js
+++ NASA-project/server/src/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import planetsRouter from './routes/planets/planets.router.js';
+import cors from 'cors';
 
 const app = express();
 
 // Middleware
+app.use(cors({
+    origin: 'http://localhost:3000',
+}))
 app.use(express.json());
 
 // Use planets router
