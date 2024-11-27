0dc78ec7d0a9a82c6c27ef56e98fed6f0c079056
bennydan7 <owusub243@gmail.com>
2024-11-27 17:08:01 +0000

    modified controller to retrieve and serve habitable planets data

--- NASA-project/server/src/routes/planets/planets.controller.js
+++ NASA-project/server/src/routes/planets/planets.controller.js
@@ -1,4 +1,4 @@
-import planets from '../../models/planets.model.js'; // Correct file extension
+import {planets} from '../../models/planets.model.js'; // Correct file extension
 
 export function getAllPlanets(req, res) {
     return res.status(200).json(planets);
