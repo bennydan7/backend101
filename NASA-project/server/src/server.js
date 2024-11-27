d1b176d86f25768705d3f9a24903384a91c6e44e
bennydan7 <owusub243@gmail.com>
2024-11-27 17:09:44 +0000

    modified server to display habitable planets

--- NASA-project/server/src/server.js
+++ NASA-project/server/src/server.js
@@ -1,10 +1,18 @@
 import http from 'http';
 import app from './app.js';
 
+import {loadPlanetsData} from './models/planets.model.js';
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+    await loadPlanetsData();
+
+    server.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+startServer();
