809bb6f3c83c59cf4604c890c2a062fa2ddbffa6
bennydan7 <owusub243@gmail.com>
2024-12-03 16:16:37 +0000

    Fix: Added wildcard route to handle unmatched paths and serve index.html

--- NASA-project/server/src/app.js
+++ NASA-project/server/src/app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/planets', planetsRouter);
 app.use('/launches', launchesRouter);
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
