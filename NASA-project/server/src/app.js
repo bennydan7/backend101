86c5a5e080f39fdde5cc86dca9054fd6faff8767
bennydan7 <owusub243@gmail.com>
2024-12-03 13:12:31 +0000

    update

--- NASA-project/server/src/app.js
+++ NASA-project/server/src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import planetsRouter from './routes/planets/planets.router.js';
+import launchesRouter from './routes/launches/launches.router.js';
+
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import morgan from 'morgan';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,12 +16,16 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
 }))
+
+app.use(morgan('combined'))
+
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Use planets router
 app.use('/planets', planetsRouter);
+app.use('/launches', launchesRouter);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
