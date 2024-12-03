4695d4dafe506d229e77333937523c243027dd9d
bennydan7 <owusub243@gmail.com>
2024-12-03 16:19:09 +0000

    Refactor: Renamed function to httpGetAllLaunches for clarity and updated implementation

--- NASA-project/server/src/routes/launches/launches.controller.js
+++ NASA-project/server/src/routes/launches/launches.controller.js
@@ -1,5 +1,5 @@
-import { launches } from '../../models/launches.model.js';
+import { getAllLaunches } from '../../models/launches.model.js';
 
-export function getAllLaunches(req, res) {
-  return res.status(200).json(Array.from(launches.values()));
+export function httpGetAllLaunches(req, res) {
+  return res.status(200).json(Array.from(getAllLaunches.values()));
 }
