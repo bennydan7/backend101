0ebe0d909a17d78ee9a9123d2c72eddda730be06
bennydan7 <owusub243@gmail.com>
2024-12-19 10:48:52 +0000

    update|

--- NASA-project/server/src/routes/launches/launches.router.js
+++ NASA-project/server/src/routes/launches/launches.router.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import {httpGetAllLaunches } from './launches.controller.js';
+import {httpAddNewLaunch, httpGetAllLaunches } from './launches.controller.js';
+import { launches } from '../../models/launches.model.js';
 
 const launchesRouter = express.Router();
 
 launchesRouter.get('/', httpGetAllLaunches);
+launchesRouter.post('/',httpAddNewLaunch)
 
 export default launchesRouter;
