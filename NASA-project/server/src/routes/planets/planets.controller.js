b95fc8b18bce62b6aaa65d9c6c5180e37300176c
bennydan7 <owusub243@gmail.com>
2024-12-19 10:49:20 +0000

    |UPDATE|

--- NASA-project/server/src/routes/planets/planets.controller.js
+++ NASA-project/server/src/routes/planets/planets.controller.js
@@ -1,5 +1,5 @@
 import {getAllPlanets} from '../../models/planets.model.js'; // Correct file extension
 
 export function httpgGetAllPlanets(req, res) {
-    return res.status(200).json(getAllPlanets);
+    return res.status(200).json(getAllPlanets());
 }
