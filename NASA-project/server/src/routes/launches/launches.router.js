7440ab75f1f92fb6e6dd2a0e87e1a1ade3d017c2
bennydan7 <owusub243@gmail.com>
2024-12-22 08:48:38 +0000

    Abort launch functionality

--- NASA-project/server/src/routes/launches/launches.router.js
+++ NASA-project/server/src/routes/launches/launches.router.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import {httpAddNewLaunch, httpGetAllLaunches } from './launches.controller.js';
 import { launches } from '../../models/launches.model.js';
-
+import { httpAbortLaunch } from './launches.controller.js';
 const launchesRouter = express.Router();
 
 launchesRouter.get('/', httpGetAllLaunches);
 launchesRouter.post('/',httpAddNewLaunch)
+launchesRouter.delete('/:id',httpAbortLaunch)
 
 export default launchesRouter;
