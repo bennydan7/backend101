93d9a2618769825cd26abd936b8bc9052d2b931e
bennydan7 <owusub243@gmail.com>
2024-12-22 07:19:22 +0000

    update

--- NASA-project/server/src/routes/launches/launches.controller.js
+++ NASA-project/server/src/routes/launches/launches.controller.js
@@ -11,7 +11,7 @@ export function httpGetAllLaunches(req, res) {
 export function httpAddNewLaunch(req, res) {
   const launch = req.body;
 
-  if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination){
+  if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.target){
     return res.status(400).json({
       error: 'Missing required launch property'
     });
