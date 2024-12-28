ab174240e67bcc3c0c9b1b961a62f208ce1ffc60
bennydan7 <owusub243@gmail.com>
2024-12-28 11:11:55 +0000

    added node cluster to allow multiple node threads to run

--- performance-example/server.js
+++ performance-example/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cluster from "cluster";
+import os from "os";
 
 const app = express();
 
@@ -12,14 +14,29 @@ app.get('/', (req, res) => {
     // JSON.parse('{}')
     // [5,4,5343,3].sort()
     // crypto functions in node  like hashing passwords
-    res.send('Performance Example');
+    res.send(`Performance example: ${process.pid}`);
     })
 
 app.get('/timer',(req,res)=>{
     delay(9000);
-    res.send('Ding ding ding')
+    res.send(`Ding ding ding ${process.pid}`);
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    });
+console.log('Running server.js')
+
+
+if(cluster.isMaster){
+    console.log('Master has been started')
+    const NUM_WORKERS = os.cpus().length;
+    for(let i=0; i<NUM_WORKERS; i++){
+        cluster.fork();
+    }
+}
+    else{
+        console.log('Woker process started')
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+            });
+    }
+
+
