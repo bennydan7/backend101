1df9d42b57cc3a23c237a295485daeba7df43e2f
bennydan7 <owusub243@gmail.com>
2025-01-02 13:57:49 +0000

    modified find option to exclude id and v

--- NASA-project/server/src/models/planets.model.js
+++ NASA-project/server/src/models/planets.model.js
@@ -44,7 +44,11 @@ export function loadPlanetsData() {
 }
 
 export async function getAllPlanets(){
-  return await planets.find({})
+  return await planets.find({},
+    {
+      '_id': 0, '__v': 0
+    }
+  )
 }
 
 async  function savePlanet(planet){ 
