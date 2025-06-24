9514da41f287df1354abecd01e148670c83cca10
Bernard Asiedu Owusu-Dankwah <owusub243@gmail.com>
2025-06-24 22:28:55 +0000

    implementing log in functionality

--- security-example/server.js
+++ security-example/server.js
@@ -3,13 +3,18 @@ const https = require('https')
 const path = require('path')
 const express = require('express')
 const helmet = require('helmet')
+const passport  = require('passport')
+const { Strategy } = require('passport-google-oauth20')
 
 require('dotenv').config()
 
 const PORT = 3000
+const config = {
+    CLIENT_ID : process.env.CLIENT_ID,
+    CLIENT_SECRET : process.env.CLIENT_SECRET
+}
 
 
-const app = express()
 //security related middleware
 // right at the top 
 /*  some middle to check security
@@ -17,17 +22,29 @@ const app = express()
 app.use((req, res, next) => {
     const isLoggdIn = true  // TODO
     IF (!isLoggedIn) {
-    return res.status(401)
-    next()})
+        return res.status(401)
+        next()})
+        
+        */
+       
+    
 
-*/
-
-const config = {
-    CLIENT_ID : process.env.CLIENT_ID,
-    CLIENT_SECRET : process.env.CLIENT_SECRET
+const AUTH_OPTIONS = {
+    callbackURL: 'auth/google/callback',
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET
 }
 
+function verifyCallback(accessToken, refreshToken, profile, done){
+
+}
+
+passport.use(new Strategy({AUTH_OPTIONS, verifyCallback}))
+
+const app = express()
+
 app.use(helmet())
+app.use(passport.initialize())
 
 function checkLoggedIn(req,res, next) {
     const isLoggedIn = true
